458a948e9c64331e961bb5c734784baff72699ca
Dane Lipscombe <dane@lipscombe.com.au>
2019-08-17 16:32:17 +1000

    Add tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,5 +2,26 @@
 
 @test "Should output variables with export prefix" {
   result="$(../envy.sh basic.env)"
-  [ "$result" = 'export VERSION=1.0.0a' ]
+  expected="export VERSION=1.0.0
+export ENVIRONMENT=development"
+
+  [ "$result" = "$expected" ]
+}
+
+
+@test "Should ignore existing environment variables" {
+  export VERSION=2.0.0
+  result="$(../envy.sh basic.env)"
+  expected="export ENVIRONMENT=development"
+
+  [ "$result" = "$expected" ]
+}
+
+@test "Should load includes" {
+  result="$(../envy.sh include.env)"
+  expected="export VERSION=1.0.0
+export ENVIRONMENT=development
+export EXTRA=dane"
+
+  [ "$result" = "$expected" ]
 }
