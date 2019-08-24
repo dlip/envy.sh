df721a6c375cc8cff51c302d4a0de35fac51757b
Dane Lipscombe <dane@lipscombe.com.au>
2019-08-25 09:34:29 +1000

    Add templating

--- envy.sh
+++ envy.sh
@@ -8,6 +8,70 @@ OUTPUT="${2:-bash}"
 ENV_NAMESPACE="__ENVY_"
 ENVY_EXPORT_EXISTING_ENV="${ENVY_EXPORT_EXISTING_ENV:-true}"
 
+mush () {
+  local SELF="$0"
+  local NULL=/dev/null
+  local STDIN=0
+  local STDOUT=1
+  local STDERR=2
+  local LEFT_DELIM="{{"
+  local RIGHT_DELIM="}}"
+  local INDENT_LEVEL="  "
+  local ESCAPE=0
+  local ENV="`env`"
+  local out=">&$STDOUT"
+
+  ## read each line
+  while IFS= read -r line; do
+    printf '%q\n' "${line}" | {
+        ## read each ENV variable
+        echo "$ENV" | {
+          while read var; do
+            ## split each ENV variable by '='
+            ## and parse the line replacing
+            ## occurrence of the key with
+            ## guarded by the values of
+            ## `LEFT_DELIM' and `RIGHT_DELIM'
+            ## with the value of the variable
+            case "$var" in
+              (*"="*)
+                key=${var%%"="*}
+                val=${var#*"="*}
+                ;;
+
+              (*)
+                key=$var
+                val=
+                ;;
+            esac
+
+            # Remove namespace for this search
+            key=$(sed "s/^${ENV_NAMESPACE}//" <<< "${key}")
+
+            line="${line//${LEFT_DELIM}$key${RIGHT_DELIM}/$val}"
+          done
+
+          if [ "1" = "$ESCAPE" ]; then
+            line="${line//&/&amp;}"
+            line="${line//\"/&quot;}"
+            line="${line//\</&lt;}"
+            line="${line//\>/&gt;}"
+          fi
+
+          ## output to stdout
+          echo "$line" | {
+            ## parse undefined variables
+            sed -e "s#${LEFT_DELIM}[A-Za-z]*${RIGHT_DELIM}##g" | \
+            ## parse comments
+            sed -e "s#${LEFT_DELIM}\!.*${RIGHT_DELIM}##g" | \
+            ## escaping
+            sed -e 's/\\\"/""/g'
+          };
+        }
+    };
+  done
+}
+
 envy() {
     INPUT=$1
     IS_LOCAL_FILE=true
@@ -31,6 +95,10 @@ envy() {
             # If variable not already set then export
             EXISTING_VAR=$(printenv "${ENV_NAMESPACE}${K}" || true)
             if [ -z "${EXISTING_VAR}" ]; then
+                # Check if templating
+                if grep -q "{{.*}}" <<< "${V}"; then
+                    V=$(mush <<< "${V}")
+                fi
                 BASH_ESCAPED_VALUE=$(sed 's/\([$\\ ]\)/\\\1/g' <<< "${V}")
                 eval "export ${ENV_NAMESPACE}${K}=${BASH_ESCAPED_VALUE}"
                 EXISTING_ENV_VAR=$(printenv "${K}" || true)
